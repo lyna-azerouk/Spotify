fda3402eb6844052b9bff5d4811d5ed4378bbd45
Lyna <azerouklyna2001@hotmail.com>
2025-05-08 12:10:27 +0200

    Stop tracking .env

--- spotify_api/spotify_auth.py
+++
@@ -1,4 +0,0 @@
-curl -X POST "https://accounts.spotify.com/api/token" \
-     -H "Authorization: Basic YTlmMTg3N2IwZjA2NDNkZGE0YTExZmEzZjE3YzdjNDE6OGI3N2I1MDFkN2VhNDdhM2EwODMyZWM4MDA1MDYwZTQ=" \
-     -H "Content-Type: application/x-www-form-urlencoded" \
-     -d "grant_type=client_credentials"
